6a0c5d29f1978d921fb07e67a1a9d80287b0b76b
adrts-iscte <58140205+adrts-iscte@users.noreply.github.com>
2023-08-12 21:38:58 +0000

    Atualizar o BackCasa.py

--- BackCasa.py
+++ BackCasa.py
@@ -33,6 +33,7 @@ def main():
             page.goto(url)
             page.wait_for_timeout(1000)
             today_date = datetime.date.today()
++ datetime.timedelta(days=1)            
             # today_date = "2023-04-15"
             tomorrow_date = today_date + datetime.timedelta(days=1)
             # tomorrow_date = "2023-04-16"
